847402ce93f5bdddc0ecaba53f14fdb0b9321362
Israel <isltimi1000@gmail.com>
2025-11-07 13:36:36 +0100

    fix: add select arrows

--- src/components/wizard/Field.jsx
+++ src/components/wizard/Field.jsx
@@ -22,14 +22,30 @@ export function Input(props) {
 
 export function Select({ children, invalid, ...rest }) {
   return (
-    <select
-      {...rest}
-      className={`w-full rounded-full border px-5 py-5 bg-white shadow-sm focus:ring-2 focus:ring-orange-300 ${
-        invalid ? "border-red-400" : "border-gray-200"
-      }`}
-    >
-      {children}
-    </select>
+    <div className="relative">
+      <select
+        {...rest}
+        className={`w-full appearance-none rounded-full border px-5 py-5 bg-white shadow-sm focus:ring-2 focus:ring-orange-300 ${
+          invalid ? "border-red-400" : "border-gray-200"
+        }`}
+      >
+        {children}
+      </select>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="pointer-events-none absolute right-7 top-1/2 h-3.5 w-3.5 -translate-y-1/2 text-gray-700"
+        viewBox="0 0 14 8"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M13.4727 0.500845L6.98599 6.96484L0.499323 0.500845" />
+      </svg>
+      
+    </div>
+    
   );
 }
 
